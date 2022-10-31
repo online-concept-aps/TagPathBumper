24eb349c45eadaf3adcd0eb303addaa16f4e9cb7
Klaus Pedersen <klaus.pedersen@onlineconcept.dk>
2022-10-31 13:11:37 +0100

    14 test

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var myargs = Environment.GetCommandLineArgs();
 var version = "customer-1.0.0";
-Console.WriteLine("Hello, World! Length" + myargs.Length);   
+Console.WriteLine("Hello, World! Length" + myargs.Length);
 
-Console.WriteLine($"::set-output name=new-tag::{version}");
+Console.Out.WriteLine($"::set-output name=new-tag::{version}");
